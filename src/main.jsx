ebada1fd5e0970188402e9f4e9b3969a79fc6bf8
ayushopchauhan <81987706+ayushopchauhan@users.noreply.github.com>
2026-04-05 13:43:27 +0530

    Add error boundary to prevent blank screen on missing data

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
+import { StrictMode, Component } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { hasError: false }
+  }
+  static getDerivedStateFromError() {
+    return { hasError: true }
+  }
+  componentDidCatch(error, info) {
+    console.error('App crash:', error, info)
+  }
+  render() {
+    if (this.state.hasError) {
+      return this.props.children
+    }
+    return this.props.children
+  }
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 )
